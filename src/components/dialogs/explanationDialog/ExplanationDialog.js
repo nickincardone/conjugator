0b28b5831c0cb811a7862caeb0a52b9910cf1e6c
Nick Incardone <nickincardone@gmail.com>
2020-04-22 16:23:10 -0400

    reason modal styling

--- src/components/dialogs/explanationDialog/ExplanationDialog.js
+++ src/components/dialogs/explanationDialog/ExplanationDialog.js
@@ -3,20 +3,46 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import './ExplanationDialog.scss';
 import { Hidden } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
+
+function formatPipes(str) {
+  let styledJSX = [];
+  const splitArray = str.split('|');
+
+  // should be even number of pipes
+  if (splitArray.length % 2 === 0) {
+    return str.replace(/\|/g, '')
+  }
+
+  for (let i = 0; i < splitArray.length; i++) {
+    if (i % 2 === 0) {
+      styledJSX.push(<React.Fragment key={i}>{splitArray[i]}</React.Fragment>)
+    } else {
+      styledJSX.push(<span key={i} className="nji-rule-span">{splitArray[i]}</span>)
+    }
+  }
+  return styledJSX;
+}
 
 function explanationDialog(props) {
 
   return (
     <Dialog onClick={props.handleClose} aria-labelledby="simple-dialog-title" open={props.open}>
-      <Typography variant="h4">{props.rule.rule}</Typography>
-      <Typography variant="subtitle1">{props.rule.example}</Typography>
-      <Typography variant="subtitle2">{props.rule.translation}</Typography>
-      <Hidden smUp>
-        <Typography variant="caption">Touch anywhere to continue</Typography>
-      </Hidden>
-      <Hidden mdDown>
-        <Typography variant="caption">Press enter to continue</Typography>
-      </Hidden>
+      <div className="nji-explanation-body">
+        <h1>Explanation</h1>
+        <h2>Rule: {props.rule.rule}</h2>
+        <Tooltip classes={{tooltip: 'nji-explanation-tooltip'}} placement="bottom" title={props.rule.translation}>
+          <div><b>Example</b>: <span className="nji-example">{formatPipes(props.rule.example)}</span></div>
+        </Tooltip>
+        <div className="nji-caption">
+        <Hidden lgUp>
+          <Typography variant="caption">Touch anywhere to continue</Typography>
+        </Hidden>
+        <Hidden mdDown>
+          <Typography variant="caption">Press enter to continue</Typography>
+        </Hidden>
+        </div>
+      </div>
     </Dialog>
   );
 }
