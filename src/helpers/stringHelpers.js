32a95353c7dd711b03a0accfe7509c28b991c430
Nick Incardone <nickincardone@gmail.com>
2020-05-01 11:03:43 -0400

    updated string diff checker to use better algorithm. Left old algorithm for future reference

--- src/helpers/stringHelpers.js
+++ src/helpers/stringHelpers.js
@@ -1,3 +1,5 @@
+import { diffChars } from  'diff'
+
 function splitFirst(str, separator) {
   let [first, ...rest] = str.split(separator);
   rest = rest.join(separator);
@@ -73,7 +75,9 @@ function getStringDifferenceArray(str, stringDifference, noMatchNumber, answerAr
   return answerArray;
 }
 
-function getStringDifferenceArrays(targetString, inputString) {
+
+
+function _getStringDifferenceArrays(targetString, inputString) { //leaving my own implementation for reference
   if (targetString === null || typeof targetString !== 'string') return null;
   if (inputString === null || inputString === '') return [[[1, targetString]], []];
   if (typeof inputString !== 'string') return null;
@@ -85,6 +89,24 @@ function getStringDifferenceArrays(targetString, inputString) {
 
   return [targetArray, inputArray];
 
+}
+
+function getStringDifferenceArrays(targetString, inputString) {
+  const diffArray = diffChars(targetString, inputString, {ignoreCase: true});
+
+  const [targetArray, inputArray] = [[],[]];
+  diffArray.forEach(diff => {
+    if (diff.added) {
+      inputArray.push([-1, diff.value]);
+    } else if (diff.removed) {
+      targetArray.push([1, diff.value]);
+    } else {
+      targetArray.push([0, diff.value]);
+      inputArray.push([0, diff.value]);
+    }
+  });
+
+  return [targetArray, inputArray];
 }
 
 export {getStringDifferenceArrays}
