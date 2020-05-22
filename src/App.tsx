fe278e86edb4a139f6899f406c6fb838855a63d0
Nick Incardone <nickincardone@gmail.com>
2020-05-22 16:44:44 -0400

    refactored app from class component to functional component with react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useCallback, useMemo, useState} from 'react';
 import './App.scss';
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
@@ -6,183 +6,161 @@ import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import OptionPage from './components/options/OptionPage';
 import Home from './components/home/Home';
-import {Question, QuestionType} from "./types";
+import {Question, QuestionType, VerbType} from "./types";
 import Settings from "./structures/Settings";
 import Quiz from "./structures/Quiz";
 import { Route, Switch, withRouter, RouteComponentProps } from 'react-router-dom';
 import QuizSection from "./components/quizSection/QuizSection";
 import Results from "./components/results/Results";
 
-interface AppState {
-  currentQuestion: Question;
-  started: boolean;
-  settings: Settings;
-}
+function App(props: RouteComponentProps) {
+  const quiz: Quiz = useMemo<Quiz>(() => new Quiz(), []);
+  const [currentQuestion, setCurrentQuestion] = useState<Question>(new Question());
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+  const [settings, setSettings] = useState<Settings>(new Settings());
 
-class App extends React.Component<RouteComponentProps, AppState> {
-  quiz: Quiz = new Quiz();
+  const questionTypes: QuestionType[] = useMemo(() => {
+    const questionTypes = [];
+    if (settings.conjugationMC) questionTypes.push(QuestionType.ConjugationMC);
+    if (settings.conjugationW && !settings.isMobile) questionTypes.push(QuestionType.ConjugationW);
+    if (settings.definitionMC) questionTypes.push(QuestionType.DefinitionMC);
+    if (settings.definitionW && !settings.isMobile) questionTypes.push(QuestionType.DefinitionW);
+    if (settings.poropara) questionTypes.push(QuestionType.PorOParaFIB);
+    return questionTypes
+  }, [settings]);
 
-  constructor(props: RouteComponentProps) {
-    super(props);
-    this.state = {
-      currentQuestion: new Question(),
-      started: false,
-      settings: new Settings(),
-    };
-  }
-
-  updateVerbTypes = (event: ChangeEvent<HTMLInputElement>) => {
-    const eventTarget = event.target;
-    this.setState((oldState: any) => {
-      const settingsCopy = { ...oldState.settings };
+  const updateVerbTypes = (event: ChangeEvent<HTMLInputElement>) => {
+    const eventTargetName = event.target.name as VerbType;
+    setSettings(prevSettings => {
+      const settingsCopy = { ...prevSettings };
       const verbTypesCopy = [...settingsCopy.verbTypes];
-      const index = verbTypesCopy.indexOf(eventTarget.name);
+      const index = verbTypesCopy.indexOf(eventTargetName);
       if (index === -1) {
-        verbTypesCopy.push(eventTarget.name)
+        verbTypesCopy.push(eventTargetName)
       } else {
         verbTypesCopy.splice(index, 1)
       }
       settingsCopy.verbTypes = verbTypesCopy;
-      return { settings: settingsCopy }
+      return settingsCopy;
     });
   };
 
-  updateSettingsEvent = (event: ChangeEvent<HTMLInputElement>) => {
-    const eventTarget = event.target;
-    this.updateSettings(eventTarget.name as keyof Settings, eventTarget.checked);
-  };
-
-  updateSettings = (settingKey: keyof Settings, settingValue: any) => {
-    this.setState((oldState) => {
-      const settingsCopy = { ...oldState.settings };
+  const updateSettings = (settingKey: keyof Settings, settingValue: any) => {
+    setSettings(prevSettings => {
+      const settingsCopy = { ...prevSettings };
       // @ts-ignore
       settingsCopy[settingKey] = settingValue as any;
-      return { settings: settingsCopy }
+      return settingsCopy
     });
   };
 
-  getQuestionTypes(): QuestionType[] {
-    const questionTypes: QuestionType[] = [];
-    if (this.state.settings.conjugationMC) questionTypes.push(QuestionType.ConjugationMC);
-    if (this.state.settings.conjugationW && !this.state.settings.isMobile) questionTypes.push(QuestionType.ConjugationW);
-    if (this.state.settings.definitionMC) questionTypes.push(QuestionType.DefinitionMC);
-    if (this.state.settings.definitionW && !this.state.settings.isMobile) questionTypes.push(QuestionType.DefinitionW);
-    if (this.state.settings.poropara) questionTypes.push(QuestionType.PorOParaFIB);
-    return questionTypes;
-  }
-
-  nextQuestion = () => {
-    const nextQuestion = this.quiz.nextQuestion() as Question;
-    this.setState({currentQuestion: nextQuestion});
+  const updateSettingsEvent = (event: ChangeEvent<HTMLInputElement>) => {
+    const eventTarget = event.target;
+    updateSettings(eventTarget.name as keyof Settings, eventTarget.checked);
   };
 
-  restart = () => {
-    this.props.history.replace("/");
+  const nextQuestion = () => {
+    const nextQuestion = quiz.nextQuestion() as Question;
+    setCurrentQuestion(nextQuestion);
   };
 
-  goToResults = () => {
-    this.props.history.replace("/results");
+  const restart = () => {
+    props.history.replace("/");
   };
 
-  processNext = (value: string) => {
-    if (this.realAnswer() !== value.toLowerCase()) {
-      this.quiz.incorrectAnswers.push({
-        ...this.state.currentQuestion,
+  const goToResults = () => {
+    props.history.replace("/results");
+  };
+
+  const realAnswer = () => {
+    return currentQuestion.answer.replace(/\|/g, '').toLowerCase();
+  };
+
+  const processNext = (value: string) => {
+    if (realAnswer() !== value.toLowerCase()) {
+      quiz.incorrectAnswers.push({
+        ...currentQuestion,
         response: value
       });
     }
-    if (this.quiz.isEnd()) {
-      this.restart();
+    if (quiz.isEnd()) {
+      restart();
     } else {
-      this.nextQuestion();
+      nextQuestion();
     }
   };
 
-  realAnswer() {
-    return this.state.currentQuestion.answer.replace(/\|/g, '').toLowerCase();
-  }
-
-  start = (isMobile: boolean) => {
-    if (this.getQuestionTypes().length === 0) return;
-    const settings = {...this.state.settings};
-    settings.isMobile = isMobile;
-    const firstQuestion = this.quiz.generateQuiz(settings);
-    this.setState({
-      settings: settings,
-      currentQuestion: firstQuestion,
-      started: true
-    }, () => this.props.history.replace('/quiz'));
+  const start = (isMobile: boolean) => {
+    if (questionTypes.length === 0) return;
+    const settingsCopy: Settings = { ...settings };
+    settingsCopy.isMobile = isMobile;
+    const firstQuestion = quiz.generateQuiz(settingsCopy);
+    setSettings(settingsCopy);
+    setCurrentQuestion(firstQuestion);
+    setIsStarted(true);
+    props.history.replace('/quiz')
   };
   
-  goToOptions = (isMobile: boolean) => {
-    const settings = {...this.state.settings};
-    settings.isMobile = isMobile;
+  const goToOptions = (isMobile: boolean) => {
+    const settingsCopy = {...settings};
+    settingsCopy.isMobile = isMobile;
     if (isMobile) {
-      settings.conjugationW = false;
-      settings.definitionW = false;
+      settingsCopy.conjugationW = false;
+      settingsCopy.definitionW = false;
     }
-    this.setState({ settings: settings }, () =>
-      this.props.history.replace("/options")
-    );
+    setSettings(settingsCopy);
+    props.history.replace("/options");
   };
 
-  checkboxChange = (event: any) => {
-    const setting = event.target.name as keyof Settings;
-    // @ts-ignore
-    this.setState({ [setting]: event.target.checked });
+  const sliderChange = (event: any, newValue: number|number[]) => {
+    updateSettings('numberOfQuestions', newValue);
   };
 
-  sliderChange = (event: any, newValue: number|number[]) => {
-    this.updateSettings('numberOfQuestions', newValue);
-  };
-
-  getAppClass = () => {
-    return (this.props.location.pathname === '/options')
+  const getAppClass = () => {
+    return (props.location.pathname === '/options')
       ? 'center-grid nji-option-mobile' : 'center-grid';
   };
 
-  render() {
-    return (
-      <Container maxWidth="md" className="nji-main">
-        <Grid container className={this.getAppClass()} direction="column">
-          <Grid item className="nji-wrap">
-            <Card className="nji-main-card">
-              <CardContent>
-                <Switch>
-                  <Route path="/results" exact render={(props => {
-                    return <Results history={props.history} results={this.quiz.incorrectAnswers}/>
-                  })} />
-                  <Route path="/options" exact render={(props => {
-                    return <OptionPage
-                      settings={this.state.settings}
-                      settingsChanged={this.updateSettingsEvent}
-                      start={this.start}
-                      sliderChange={this.sliderChange}
-                      updateVerbTypes={this.updateVerbTypes}/>
-                  })}/>
-                  <Route path="/quiz" exact render={(props => {
-                    return <QuizSection next={this.processNext}
-                                        history={props.history}
-                                        question={this.state.currentQuestion}
-                                        percentComplete={(this.quiz.currentQuestion / this.state.settings.numberOfQuestions) * 100}/>
-                  })}/>
-                  <Route path="/" render={(props => {
-                    return <Home
-                      numberOfQuestions={this.state.settings.numberOfQuestions}
-                      incorrectAnswers={this.quiz.incorrectAnswers.length}
-                      start={this.start}
-                      goToOptions={this.goToOptions}
-                      goToResults={this.goToResults}
-                      started={this.state.started}/>
-                  })}/>
-                </Switch>
-              </CardContent>
-            </Card>
-          </Grid>
+  return (
+    <Container maxWidth="md" className="nji-main">
+      <Grid container className={getAppClass()} direction="column">
+        <Grid item className="nji-wrap">
+          <Card className="nji-main-card">
+            <CardContent>
+              <Switch>
+                <Route path="/results" exact render={(props => {
+                  return <Results history={props.history} results={quiz.incorrectAnswers}/>
+                })} />
+                <Route path="/options" exact render={(props => {
+                  return <OptionPage
+                    settings={settings}
+                    settingsChanged={updateSettingsEvent}
+                    start={start}
+                    sliderChange={sliderChange}
+                    updateVerbTypes={updateVerbTypes}/>
+                })}/>
+                <Route path="/quiz" exact render={(props => {
+                  return <QuizSection next={processNext}
+                                      history={props.history}
+                                      question={currentQuestion}
+                                      percentComplete={(quiz.currentQuestion / settings.numberOfQuestions) * 100}/>
+                })}/>
+                <Route path="/" render={(props => {
+                  return <Home
+                    numberOfQuestions={settings.numberOfQuestions}
+                    incorrectAnswers={quiz.incorrectAnswers.length}
+                    start={start}
+                    goToOptions={goToOptions}
+                    goToResults={goToResults}
+                    started={isStarted}/>
+                })}/>
+              </Switch>
+            </CardContent>
+          </Card>
         </Grid>
-      </Container>
-    );
-  }
+      </Grid>
+    </Container>
+  );
 }
 
 export default withRouter(App);
